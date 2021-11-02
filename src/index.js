695e8a161a6e15420ea45bb194f77e55895cf18a
fandee <pavlo.protsyshyn@gmail.com>
2021-11-02 19:07:26 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+//import App from './App1'
+//import './index1.css'
 
 ReactDOM.render(<App/>, document.getElementById('root'))
